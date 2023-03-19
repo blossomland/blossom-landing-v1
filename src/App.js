305d647452f24c54836a78e372447915cefcdfa3
pentacle <pentacle@pentacle.xyz>
2023-03-19 09:54:26 +0000

    feat: center text

--- src/App.js
+++ src/App.js
@@ -28,9 +28,8 @@ function App() {
             <header className="App-header p-8">
                 {/*<img src={logo} className="App-logo" alt="logo" />*/}
                 <div className={"text-xs"}>
-                    <p className={"uppercase"}>Blossom Land </p>
-                    <p className={"pt-8"}>Get notifications: </p>
-                    <p className={"py-2 uppercase"}><a href={"HTTPS://T.ME/+YC1DIWBEHYAYM2VK"}>https://t.me/+yc1dIWBeHyAyM2Vk</a></p>
+                    <p className={"uppercase text-center"}>Blossom Land ~ soon™</p>
+
                 </div>
             </header>
 
@@ -49,7 +48,8 @@ function App() {
                 </div>
             </article>
 
-            <footer className={"text-xs h-32 w-full mx-auto text-center p-8"}>~ soon™</footer>
+            <footer className={"text-xs h-32 w-full mx-auto text-center dotted-01"}><p className={"pt-8"}>Get notifications: </p>
+                <p className={"py-2 uppercase"}><a href={"HTTPS://T.ME/+YC1DIWBEHYAYM2VK"}>https://t.me/+yc1dIWBeHyAyM2Vk</a></p></footer>
         </section>
     );
 }
